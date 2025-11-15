c61cf14a9af95373d3ef430f788208a4c0088eb6
Kurasao <jamerolenor@gmail.com>
2025-11-15 18:44:35 +0800

    set up api based controller, sanctum installation and set up, validations using laravel request classes
    - php artisan make:controller [Name]Controller --api
    --api = api resource controller without create or edit methods
    --resource = create crud routes
    
    - php artisan install:api
    
    - php artisan make:request [Method][Name]Request

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAuthorRequest;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $authors = Author::paginate(10); // or all()
+
+        return response()->json([
+            'author' => $authors,
+            'message' => 'Authors Fetched Successfully',
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAuthorRequest $request)
+    {
+        $author = Author::create($request->validated());
+
+        return response()->json([
+            'author' => $author,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
